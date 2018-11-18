13f628236b75c74fc69c5904490085d68c477526
Tressa S <39808139+Tressa-Sanders@users.noreply.github.com>
2018-11-18 17:11:49 -0500

    Update calendar.rb

--- lib/simple_calendar/calendar.rb
+++ lib/simple_calendar/calendar.rb
@@ -58,11 +58,11 @@ module SimpleCalendar
     end
 
     def url_for_next_view
-      view_context.url_for(@params.merge(start_date_param => date_range.last + 1.day))
+      view_context.url_for(@params.merge(start_date_param => (date_range.last + 1.day).strftime("%m/%Y")))
     end
 
     def url_for_previous_view
-      view_context.url_for(@params.merge(start_date_param => date_range.first - 1.day))
+      view_context.url_for(@params.merge(start_date_param => (date_range.first - 1.day).strftime("%m/%Y")))
     end
 
     def date_range
